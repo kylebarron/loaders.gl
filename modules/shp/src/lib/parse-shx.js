404706a6d0e70f2b8d9bd0e1faa0f76a52fe42dd
Kyle Barron <kylebarron2@gmail.com>
2020-06-17 18:04:22 -0600

    Simplify parsing shx

--- modules/shp/src/lib/parse-shx.js
+++ modules/shp/src/lib/parse-shx.js
@@ -1,40 +1,28 @@
-/* eslint-disable */
+import {parseHeader, BIG_ENDIAN} from './util';
 // var {readFileSync} = require('fs');
 // var path = '../../shp/PADUS2_0CO_Shapefile/PADUS2_0Proclamation_CO.shx';
 // var arrayBuffer = readFileSync(path).buffer;
+// readFileSync(path).length;
 
-var SHX_HEADER_SIZE = 100;
+const SHX_HEADER_SIZE = 100;
 
-var LITTLE_ENDIAN = true;
-var BIG_ENDIAN = false;
+export function parseShx(arrayBuffer) {
+  const headerView = new DataView(arrayBuffer, 0, SHX_HEADER_SIZE);
+  const header = parseHeader(headerView);
+  const contentLength = header.length - SHX_HEADER_SIZE;
 
-function parseShx(arrayBuffer) {
-  // var headerView = new DataView(arrayBuffer, 0, SHX_HEADER_SIZE);
-  // var header = parseHeader(headerView);
+  const contentView = new DataView(arrayBuffer, SHX_HEADER_SIZE, contentLength);
 
-  var contentView = new DataView(arrayBuffer, SHX_HEADER_SIZE);
+  const offsets = new Int32Array(contentLength);
+  const lengths = new Int32Array(contentLength);
 
-  // Note: this byteLength is incorrect for small buffers in Node; it'll always show 8192
-  // https://github.com/protobufjs/protobuf.js/issues/852
-  var {byteLength} = contentView;
-  var byteOffsets = new Int32Array(byteLength);
-  var byteLengths = new Int32Array(byteLength);
-
-  var recordIndex = 0;
-  while (true) {
-    var recordOffset = contentView.getInt32(recordIndex * 8, BIG_ENDIAN);
-    var recordByteLength = contentView.getInt32(recordIndex * 8 + 4, BIG_ENDIAN);
-    if (recordOffset === 0) {
-      break;
-    }
-
-    byteOffsets[recordIndex] = recordOffset;
-    byteLengths[recordIndex] = recordByteLength;
-    recordIndex++;
+  for (let i = 0; i < contentLength / 8; i++) {
+    offsets[i] = contentView.getInt32(i * 8, BIG_ENDIAN);
+    lengths[i] = contentView.getInt32(i * 8 + 4, BIG_ENDIAN);
   }
 
   return {
-    offsets: byteOffsets.subarray(0, recordIndex),
-    lengths: byteLengths.subarray(0, recordIndex)
+    offsets,
+    lengths
   };
 }
