2ba52e2014d9ea7429d16348f41ae7c7d3472655
Kyle Barron <kylebarron2@gmail.com>
2020-06-18 14:34:03 -0600

    Simplify parsing positions to support future 3/4D parsing

--- modules/shp/src/lib/parse-geometry.js
+++ modules/shp/src/lib/parse-geometry.js
@@ -1,5 +1,6 @@
 import {LITTLE_ENDIAN} from './util';
 
+// eslint-disable-next-line complexity
 export function parseRecord(view) {
   let offset = 0;
   const type = view.getInt32(offset, LITTLE_ENDIAN);
@@ -11,21 +12,29 @@ export function parseRecord(view) {
     case 1:
       return parsePoint(view, offset, 2);
     case 3:
-      return parsePoly(view, offset);
+      return parsePoly(view, offset, 2);
     case 5:
-      return parsePoly(view, offset);
+      return parsePoly(view, offset, 2);
     case 8:
-      return parseMultiPoint(view, offset);
+      return parseMultiPoint(view, offset, 2);
+    // GeometryZ can have 3 or 4 dimensions, since the M is not required to
+    // exist
     case 11:
       return parsePoint(view, offset, 4); // PointZ
-    // case 13: return parsePolyLine(view, offset); // PolyLineZ
-    // case 15: return parsePolygon(view, offset); // PolygonZ
-    // case 18: return parseMultiPoint(view, offset); // MultiPointZ
+    // case 13:
+    //   return parsePoly(view, offset, 4); // PolyLineZ
+    // case 15:
+    //   return parsePoly(view, offset, 4); // PolygonZ
+    // case 18:
+    //   return parseMultiPoint(view, offset, 4); // MultiPointZ
     case 21:
       return parsePoint(view, offset, 3); // PointM
-    // case 23: return parsePolyLine(view, offset); // PolyLineM
-    // case 25: return parsePolygon(view, offset); // PolygonM
-    // case 28: return parseMultiPoint(view, offset);// MultiPointM
+    // case 23:
+    //   return parsePoly(view, offset, 3); // PolyLineM
+    // case 25:
+    //   return parsePoly(view, offset, 3); // PolygonM
+    // case 28:
+    //   return parseMultiPoint(view, offset, 3); // MultiPointM
     default:
       throw new Error(`unsupported shape type: ${type}`);
   }
@@ -36,25 +45,37 @@ function parseNull(view, offset) {
 }
 
 function parsePoint(view, offset, dim) {
-  const bufferOffset = view.byteOffset + offset;
-  const bufferLength = dim * Float64Array.BYTES_PER_ELEMENT;
-  return new Float64Array(view.buffer.slice(bufferOffset, bufferOffset + bufferLength));
+  let positions;
+  [positions, offset] = parsePositions(view, offset, 1, dim);
+
+  return {
+    positions: {value: positions, size: dim},
+    type: 'Point'
+  };
 }
 
-function parseMultiPoint(view, offset) {
+function parseMultiPoint(view, offset, dim) {
   // skip parsing box
   offset += 4 * Float64Array.BYTES_PER_ELEMENT;
 
   const nPoints = view.getInt32(offset, LITTLE_ENDIAN);
   offset += Int32Array.BYTES_PER_ELEMENT;
 
-  return parse2dPositions(view, offset, nPoints);
+  let xyPositions;
+  [xyPositions, offset] = parsePositions(view, offset, nPoints, 2);
+
+  // TODO: parse 3/4D, concat and interleave
+
+  return {
+    positions: {value: xyPositions, size: dim},
+    type: 'Point'
+  };
 }
 
 // MultiPolygon doesn't exist? Multiple records with the same attributes?
 // polygon and polyline parsing
 // This is 2d only
-function parsePoly(view, offset) {
+function parsePoly(view, offset, dim) {
   // skip parsing bounding box
   offset += 4 * Float64Array.BYTES_PER_ELEMENT;
 
@@ -70,14 +91,20 @@ function parsePoly(view, offset) {
   const indices = new Int32Array(view.buffer.slice(bufferOffset, bufferOffset + bufferLength));
   offset += nParts * Int32Array.BYTES_PER_ELEMENT;
 
+  let xyPositions;
+  [xyPositions, offset] = parsePositions(view, offset, nPoints, 2);
+
+  // TODO: parse 3/4D, concat and interleave
+
   return {
-    positions: parse2dPositions(view, offset, nPoints),
-    indices
+    positions: {value: xyPositions, size: dim},
+    indices: {value: indices, size: 1}
   };
 }
 
-function parse2dPositions(view, offset, nPoints) {
+// Parse a contiguous block of positions into a Float64Array
+function parsePositions(view, offset, nPoints, dim) {
   const bufferOffset = view.byteOffset + offset;
-  const bufferLength = nPoints * 2 * Float64Array.BYTES_PER_ELEMENT;
-  return new Float64Array(view.buffer.slice(bufferOffset, bufferOffset + bufferLength));
+  const bufferLength = nPoints * dim * Float64Array.BYTES_PER_ELEMENT;
+  return [new Float64Array(view.buffer.slice(bufferOffset, bufferOffset + bufferLength)), offset];
 }
