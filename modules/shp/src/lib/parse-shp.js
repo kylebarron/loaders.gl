8a0aaa12b8d11a3d3a93522b2e66dd7fb11ab5bd
Kyle Barron <kylebarron2@gmail.com>
2020-06-18 13:52:10 -0600

    Adopt bostock's skip behavior

--- modules/shp/src/lib/parse-shp.js
+++ modules/shp/src/lib/parse-shp.js
@@ -3,16 +3,17 @@ import {parseRecord} from './parse-geometry';
 
 
 const SHAPE_HEADER_SIZE = 100;
-// In line with the spec; the record header is just index, byte length
-// geometry type is part of the record
-const SHAPE_RECORD_HEADER_SIZE = 8;
+// According to the spec, the record header is just 8 bytes, but here we set it
+// to 12 so that we can also access the record's type
+const SHAPE_RECORD_HEADER_SIZE = 12;
 
 export default function parseShape(arrayBuffer) {
   const headerView = new DataView(arrayBuffer, 0, SHAPE_HEADER_SIZE);
   const header = parseHeader(headerView);
 
   // eslint-disable-next-line
-  let currentIndex = 0;
+  // index numbering starts at 1
+  let currentIndex = 1;
   const features = [];
 
   let offset = SHAPE_HEADER_SIZE;
@@ -24,24 +25,23 @@ export default function parseShape(arrayBuffer) {
     const recordNumber = recordHeaderView.getInt32(0, BIG_ENDIAN);
     // 2 byte words; includes the four words of record header
     const byteLength = recordHeaderView.getInt32(4, BIG_ENDIAN) * 2;
-    offset += Int32Array.BYTES_PER_ELEMENT * 2;
-    // var type = recordHeaderView.getInt32(8, true);
+    const type = recordHeaderView.getInt32(8, true);
 
-    const recordView = new DataView(arrayBuffer, offset, byteLength);
-    features.push(parseRecord(recordView));
-    currentIndex++;
-    offset += byteLength;
+    // All records must have at least four bytes (for the record shape type)
+    if (byteLength < 4 || type !== header.type || recordNumber !== currentIndex) {
+      // Malformed record, try again after advancing 4 bytes
+      offset += 4;
+    } else {
+      // Move past header
+      // type is actually part of the record, not the header, so only increase
+      // offset past recordNumber and byteLength
+      offset += Int32Array.BYTES_PER_ELEMENT * 2;
 
-    // // All records must have at least four bytes (for the record shape type)
-    // if (byteLength < 4 || type !== header.type || recordNumber !== currentIndex) {
-    //   // Malformed record, try again after advancing 4 bytes
-    //   offset += 4;
-    // } else {
-    //   var recordView = new DataView(arrayBuffer, offset + 8, 4 + length);
-    //   features.push(parseRecord(recordView));
-    //   currentIndex++;
-    //   offset += SHAPE_RECORD_HEADER_SIZE + byteLength;
-    // }
+      const recordView = new DataView(arrayBuffer, offset, byteLength);
+      features.push(parseRecord(recordView));
+      currentIndex++;
+      offset += byteLength;
+    }
   }
 
   return {
